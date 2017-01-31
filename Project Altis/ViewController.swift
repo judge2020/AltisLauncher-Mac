baa0212ded9a02c038342cba269831f48af4a32a
judge2020 <hunterray55@hotmail.com>
2017-01-31 12:23:38 -0800

    Initial Commit

---
+++ Project Altis/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Project Altis
+//
+//  Created by Hunter Ray on 1/31/17.
+//  Copyright © 2017 Judge2020. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
