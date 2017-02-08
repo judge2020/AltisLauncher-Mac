98eea26cf304bac547735e04c0fff4529fa02627
judge2020 <hunterray55@hotmail.com>
2017-02-07 18:44:50 -0800

    Unoffificial notice. download counter. better downloading

--- Project Altis/ViewController.swift
+++ Project Altis/ViewController.swift
@@ -21,6 +21,9 @@ class ViewController: NSViewController {
     let documentsDirectory = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first!
     var dataPath: URL? = nil
     
+    var total = 0
+    var done = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -47,7 +50,6 @@ class ViewController: NSViewController {
     }
 
     @IBAction func PlayPress(_ sender: Any) {
-        _StatusField.stringValue = "Checking for updates..."
         Alamofire.request("https://projectaltis.com/api/manifest").responseString{response in
             let raw = response.result.value! as String
             let array = raw.components(separatedBy: "#")
@@ -55,15 +57,12 @@ class ViewController: NSViewController {
             //handle update
             self.handleUpdate(array: array)
             
-            self.launchTT(username: self._UsermameField.stringValue, password: self._PasswordField.stringValue)
         }
     }
     @IBAction func RedownloadPress(_ sender: Any) {
         try? FileManager.default.removeItem(at: dataPath!)
         
         startup()
-        
-        _StatusField.stringValue = "Redownloading..."
         Alamofire.request("https://projectaltis.com/api/manifest").responseString{response in
             let raw = response.result.value! as String
             let array = raw.components(separatedBy: "#")
@@ -74,6 +73,7 @@ class ViewController: NSViewController {
     }
     
     func handleUpdate(array: [String]){
+        try? FileManager.default.removeItem(at: (dataPath?.appendingPathComponent("cgGL.dll"))!)
         for root in array{
             let json = JSON(data: root.data(using: .utf8)!)
             var filename = json["filename"].stringValue
@@ -89,18 +89,33 @@ class ViewController: NSViewController {
             let filepath = (self.dataPath?.appendingPathComponent(filename))!
             if (!FileManager.default.fileExists(atPath: filepath.path)){
                 print("Missing: " + filename)
-                _StatusField.stringValue = "Downloading files..."
-                Downloader.load(url: try! json["url"].stringValue.asURL(), to: filepath, completion: ({}))
+                self.total += 1
+                DispatchQueue.global(qos: .default).async {
+                    self.downloadAlamo(path: filepath, url: json["url"].stringValue)
+                }
             }
             else{
                 print("Found: " + filename)
+                //filesize checking for updates
+                let filesize = (try? FileManager.default.attributesOfItem(atPath: filepath.path) as NSDictionary)?.fileSize()
+                if (String(describing: filesize!) != json["size"].string){
+                    print("Filesize mismatch, redownloading: " + filename)
+                    self.total += 1
+                    DispatchQueue.global(qos: .default).async {
+                        try? FileManager.default.removeItem(at: filepath)
+                        self.downloadAlamo(path: filepath, url: json["url"].stringValue)
+                    }
+                }
+                
             }
         }
+        
     }
     
 
     
     func launchTT(username: String, password: String){
+        //sendNotification(title: "Project ALtis", text: "Trying to open Toontown")
         self._StatusField.stringValue = "Note: Do NOT report bugs for this to the TTPA team!"
         self._UsermameField.stringValue = "NOTE: expect extreme performance issues."
         self._PasswordField.stringValue = ""
@@ -115,6 +130,15 @@ class ViewController: NSViewController {
         setenv(name, value, overwrite ? 1 : 0)
     }
     
+    func sendNotification(title: String, text: String){
+        let notification = NSUserNotification.init()
+        notification.title = title
+        notification.informativeText = text
+        notification.soundName = NSUserNotificationDefaultSoundName
+        notification.hasActionButton = false
+        NSUserNotificationCenter.default.deliver(notification)
+    }
+    
     @discardableResult
     func shell(_ args: String...) -> Int32 {
         let task = Process()
@@ -124,35 +148,34 @@ class ViewController: NSViewController {
         task.waitUntilExit()
         return task.terminationStatus
     }
-
-}
-
-class Downloader {
-    class func load(url: URL, to localUrl: URL, completion: @escaping () -> ()) {
-        let sessionConfig = URLSessionConfiguration.default
-        let session = URLSession(configuration: sessionConfig)
-        let request = try! URLRequest(url: url, method: .get)
-        
-        let task = session.downloadTask(with: request) { (tempLocalUrl, response, error) in
-            if let tempLocalUrl = tempLocalUrl, error == nil {
-                // Success
-                if let statusCode = (response as? HTTPURLResponse)?.statusCode {
-                    print("Success: \(statusCode)")
-                }
-                
-                do {
-                    try FileManager.default.copyItem(at: tempLocalUrl, to: localUrl)
-                    completion()
-                } catch (let writeError) {
-                    print("error writing file \(localUrl) : \(writeError)")
-                }
-                
-            } else {
-                print("Failure: %@", error?.localizedDescription as Any);
-            }
+    
+    func downloadAlamo(path: URL, url: String){
+        //let destination = DownloadRequest.suggestedDownloadDestination(for: .applicationSupportDirectory)
+        let destination: DownloadRequest.DownloadFileDestination = { _, _ in
+            let documentsURL = path
+            
+            return (documentsURL, [.removePreviousFile])
         }
-        task.resume()
+        Alamofire.download(
+            url,
+            method: .get,
+            to: destination).downloadProgress(closure: { (progress) in
+                //progress closure
+            }).response(completionHandler: { (DefaultDownloadResponse) in
+                self.done += 1
+                self.updateStatus()
+            })
     }
+    
+    func updateStatus(){
+        if (self.done == self.total){
+            self._StatusField.stringValue = "finished downloading!"
+            self.launchTT(username: self._UsermameField.stringValue, password: self._PasswordField.stringValue)
+            return
+        }
+        self._StatusField.stringValue = "Downloading... " + String(self.done) + "/" + String(self.total)
+    }
+
 }
 
 class Regex {
